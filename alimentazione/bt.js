27e7eea8c523943cdb697ba4bd935eb6291ad788
lorenzo di filippo <difilippo.1837562@studenti.uniroma1.it>
2021-05-08 21:59:25 +0200

    alimentazione
    quasi finito

--- alimentazione/bt.js
+++ alimentazione/bt.js
@@ -1,6 +1,8 @@
 
 var coll_pesce = $(".collapse_pesce");
-var coll_carne = $(".collapse_carne")
+var coll_carne = $(".collapse_carne");
+var coll_farina = $(".collapse_farina");
+var coll_frutta = $(".collapse_frutta");
 
 
 function show(str){
@@ -8,22 +10,58 @@ function show(str){
         if(coll_carne.css("display")=="flex"){
             coll_pesce.css("display","none");
             coll_carne.css("display","none");
+            coll_farina.css("display","none");
+            coll_frutta.css("display","none");
         }
         else{
             coll_carne.css("display","flex");
             coll_pesce.css("display","none");
+            coll_farina.css("display","none");
+            coll_frutta.css("display","none");
         }
     }
     if(str==2){
         if(coll_pesce.css("display")=="flex"){
             coll_carne.css("display","none");
             coll_pesce.css("display","none");
+            coll_farina.css("display","none");
+            coll_frutta.css("display","none");
         }
         else{
             coll_pesce.css("display","flex");
             coll_carne.css("display","none");
+            coll_farina.css("display","none");
+            coll_frutta.css("display","none");
         }
-    }        
+    }
+    if(str==3){
+        if(coll_farina.css("display")=="flex"){
+            coll_carne.css("display","none");
+            coll_pesce.css("display","none");
+            coll_farina.css("display","none");
+            coll_frutta.css("display","none");
+        }
+        else{
+            coll_farina.css("display","flex");
+            coll_carne.css("display","none");
+            coll_pesce.css("display","none");
+            coll_frutta.css("display","none");
+        }
+    } 
+    if(str==4){
+        if(coll_frutta.css("display")=="flex"){
+            coll_carne.css("display","none");
+            coll_pesce.css("display","none");
+            coll_farina.css("display","none");
+            coll_frutta.css("display","none");
+        }
+        else{
+            coll_frutta.css("display","flex");
+            coll_carne.css("display","none");
+            coll_pesce.css("display","none");
+            coll_farina.css("display","none");
+        }
+    }       
 }
 
 
