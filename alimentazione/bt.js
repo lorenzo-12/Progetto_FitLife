194a6bb1d5f7ea5014728466d0a2d297c831d92f
lorenzo di filippo <difilippo.1837562@studenti.uniroma1.it>
2021-05-08 20:40:12 +0200

    ALIMENTAZIONE
    quasi ultimato

--- alimentazione/bt.js
+++ alimentazione/bt.js
@@ -1,12 +1,29 @@
 
-// var lca = document.getElementsByClassName("card_carne");
-// var lpe = document.getElementsByClassName("card_pesce");
-var lca = $(".collapse_carne")
-alert(lca.length);
+var coll_pesce = $(".collapse_pesce");
+var coll_carne = $(".collapse_carne")
+
 
 function show(str){
-    alert("premuto");
-    lca.css("display","none");
+    if(str==1){
+        if(coll_carne.css("display")=="flex"){
+            coll_pesce.css("display","none");
+            coll_carne.css("display","none");
+        }
+        else{
+            coll_carne.css("display","flex");
+            coll_pesce.css("display","none");
+        }
+    }
+    if(str==2){
+        if(coll_pesce.css("display")=="flex"){
+            coll_carne.css("display","none");
+            coll_pesce.css("display","none");
+        }
+        else{
+            coll_pesce.css("display","flex");
+            coll_carne.css("display","none");
+        }
+    }        
 }
 
 
