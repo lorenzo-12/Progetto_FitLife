00ce3ac8f6b616e015e64a2c49be6b7a9b79fafe
lorenzo di filippo <difilippo.1837562@studenti.uniroma1.it>
2021-05-08 11:52:36 +0200

    alimentazione
    progresso

---
+++ alimentazione/bt.js
@@ -0,0 +1,13 @@
+
+// var lca = document.getElementsByClassName("card_carne");
+// var lpe = document.getElementsByClassName("card_pesce");
+var lca = $(".collapse_carne")
+alert(lca.length);
+
+function show(str){
+    alert("premuto");
+    lca.css("display","none");
+}
+
+
+
