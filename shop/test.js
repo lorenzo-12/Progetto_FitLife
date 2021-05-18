0d01683c6105f6a9afa48b702c87e63e7b75a4bc
gianluca-cacciarini <cacciarins@gmail.com>
2021-05-18 15:09:00 +0200

    change

--- shop/test.js
+++ shop/test.js
@@ -61,7 +61,7 @@ for (let i=0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
         cartNumbers(products[i]);
         totalCost(products[i]);
-
+        displayCart();
     })
 }
 
